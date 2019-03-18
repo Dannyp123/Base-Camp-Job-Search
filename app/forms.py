0c71525fcba4ec4ea9e1f2178d8de7dc20078e9e
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2019-03-18 11:10:55 -0500

    added placeholders to the forms

--- app/forms.py
+++ app/forms.py
@@ -3,41 +3,29 @@ from django import forms
 
 class BlogPostForm(forms.Form):
     image = forms.URLField(
-        label="User Image:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "URL"}))
     title = forms.CharField(
-        label="Job Title:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Title"}))
     companyName = forms.CharField(
-        label="Company Name:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Name"}))
     author = forms.CharField(
-        label="Employer Name:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-5'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-5', 'placeholder': "Author's name"}))
     streetNum = forms.CharField(
-        label='Street number:',
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "123"}))
     streetName = forms.CharField(
-        label='Street name:',
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Evergreen Street"}))
     townName = forms.CharField(
-        label='Town Name:',
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Oakland"}))
     state = forms.CharField(
-        label='State:',
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "OK"}))
     zipCode = forms.CharField(
-        label="Zip Code: ",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-5'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-5', 'placeholder': "99834"}))
     postion = forms.CharField(
-        label="Employer's Current Postion:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Postion"}))
     benefits = forms.CharField(
-        label="Job Benefits:",
-        widget=forms.TextInput(attrs={'class': 'form-control mb-3'}))
+        widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': "Benefits"}))
     body = forms.CharField(
-        label="Post Body:",
-        widget=forms.Textarea(attrs={'class': 'form-control mb-3'}))
+        widget=forms.Textarea(attrs={'class': 'form-control mb-3', 'placeholder': "Description"}))
 
 
 class BlogCommentForm(forms.Form):
